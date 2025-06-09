42ca62f8a41ea02e026d69d00ce8be845272f340
aenodehi <aenodehi@gmail.com>
2025-06-09 20:59:05 +0330

    Remove

--- scripts/p02.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv("./data/B0005_discharge_adjusted.csv", index_col = "datetime_", parse_dates = True)
-#print(df.head())
-df['datetime'] = pd.to_datetime(df['datetime'], yearfirst=True)
-print(df.datetime.min(), df.datetime.max())
